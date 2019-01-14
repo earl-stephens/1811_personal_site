43c9f953e7e47cef604101f4957cd88d571ee120
earl-stephens <stephensearl@hotmail.com>
2019-01-14 14:08:44 -0700

    added views directory and index.html file to that directory

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,7 +4,7 @@ class PersonalSite
 
   def self.call(env)
     # require 'pry'; binding.pry
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
   end
 
 end
