9f123479effa5c25167ba6644e917d10f394fb7f
earl-stephens <stephensearl@hotmail.com>
2019-01-14 14:34:18 -0700

    broke out the self.call method into index.html and error.html methods

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 
 class HomepageTest < CapybaraTestCase
+
   def test_user_can_see_the_homepage
     visit '/'
 
@@ -9,4 +10,12 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_gets_an_error_message
+    visit '/house'
+
+    assert_equal 404, page.status_code
+    assert page.has_content?("Page not found.")
+  end
+
 end
